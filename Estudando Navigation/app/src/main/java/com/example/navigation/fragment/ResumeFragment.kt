56d563a238fda75619887896b6d50e380ee36cd5
ThLourenco <thilourenco01@gmail.com>
2025-02-24 22:29:53 -0300

    finalizando estudo navigation

--- Estudando Navigation/app/src/main/java/com/example/navigation/fragment/ResumeFragment.kt
+++ Estudando Navigation/app/src/main/java/com/example/navigation/fragment/ResumeFragment.kt
@@ -6,19 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.navigation.databinding.AdressFragmentBinding
-import com.example.navigation.databinding.FragmentPersonDetailBinding
+import com.example.navigation.databinding.FragmentResumeBinding
 import com.example.navigation.extensions.text
 import com.example.navigation.model.PersonModel
 
-class AdressFragment: Fragment() {
+class ResumeFragment: Fragment() {
 
-    private var _binding: AdressFragmentBinding?=null
+    private var _binding: FragmentResumeBinding?=null
     private val binding get() = _binding!!
 
-    private val args by navArgs<AdressFragmentArgs>()
+    private val args by navArgs<ResumeFragmentArgs>()
 
     override fun onCreateView(inflater: LayoutInflater, group: ViewGroup?, saved: Bundle?): View {
         _binding = AdressFragmentBinding.inflate(inflater, group, false)
@@ -28,20 +27,16 @@ class AdressFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Log.e("TAG","onViewCreate: ${args.model}")
+      binding.tvPersom.text = args.model.person
+        binding.tvAddres.text = args.model.address
 
-        binding.btnNext.setOnClickListener(){
-          val model =  args.model.copy(
-              street = binding.tilStreet.text,
-              number = binding.tilStreet.text.toInt()
-          )
-            val directions = AdressFragmentDirections.goToResumeFragment(model)
-            findNavController().navigate(directions)
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+
 }
